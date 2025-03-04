c74ff37aa497a752fde99d1f1774866f9a1d712b
theresaSheets <utg8@cdc.gov>
2025-03-04 12:02:51 -0500

    adding functionality with additional compartments

---
+++ scripts/simulate_measles.py
@@ -0,0 +1,64 @@
+import numpy as np
+import polars as pl
+import polars.selectors as cs
+import griddler
+import griddler.griddle
+from sir import SEIRModel
+
+def simulate(parms):
+
+    beta_2_value = None
+    if "beta_2_low" in parms and "beta_2_high" in parms:
+        beta_2_value = np.random.uniform(parms["beta_2_low"], parms["beta_2_high"])
+        parms["beta"][1][1] = beta_2_value
+
+    t = np.linspace(0, parms["tf"], parms["tl"])
+    groups = parms["n_groups"]
+    S = np.zeros((parms["tl"], groups))
+    V = np.zeros((parms["tl"], groups))
+    E1 = np.zeros((parms["tl"], groups))
+    E2 = np.zeros((parms["tl"], groups))
+    I1 = np.zeros((parms["tl"], groups))
+    I2 = np.zeros((parms["tl"], groups))
+    R = np.zeros((parms["tl"], groups))
+    Y = np.zeros((parms["tl"], groups))
+    u = [[parms["N"][group] - parms["I0"][group],
+          0,
+          parms["I0"][group],
+          0,
+          0
+         ] for group in range(groups)]
+    for group in range(groups):
+        S[0, group], V[0, group], E1[0, group], E2[0, group], I1[0, group], I2[0, group], R[0, group], Y[0, group] = u[group]
+
+    model = SEIRModel(parms)
+
+    for j in range(1, parms["tl"]):
+        u = model.seirmodel(u, t[j])
+        for group in range(groups):
+            S[j, group], V[j, group], E1[j, group], E2[j, group], I1[j, group], I2[j, group], R[j, group], Y[j, group] = u[group]
+
+    df = pl.DataFrame({
+        't': np.repeat(t, groups),
+        'group': np.tile(np.arange(groups), parms["tl"]),
+        'S': S.flatten(),
+        'V': V.flatten(),
+        'E1': E1.flatten(),
+        'E2': E2.flatten(),
+        'I1': I1.flatten(),
+        'I2': I2.flatten(),
+        'R': R.flatten(),
+        'Y': Y.flatten(),
+        'beta_2_value': [beta_2_value] * (parms["tl"] * groups)
+    })
+
+    return df
+
+if __name__ == "__main__":
+    parameter_sets = griddler.griddle.read("scripts/config.yaml")
+    results_all = griddler.run_squash(griddler.replicated(simulate), parameter_sets)
+    print(results_all.columns)
+    results = results_all.select(cs.by_name(["t", "group", "Y", "replicate", "beta_2_value"]))
+    # with pl.Config(tbl_rows = -1):
+    #     print(results)
+    results.write_csv("output/results_100_beta.csv")
---
+++ sir/__init__ measles.py
@@ -0,0 +1,54 @@
+import numpy as np
+import math
+
+class SEIRModel:
+    def __init__(self, parms):
+        self.parms = parms
+        self.groups = parms["n_groups"]
+
+    def exposed(self, u): #need old exposed eventally, old should be more infectious
+        new_exposed = []
+        # Extract the number of infected individuals for each group
+        I_g = np.array([u[group][2] for group in range(self.groups)])
+        for target_group in range(self.groups):
+            S, E1,  E2, I1, I2, R, Y = u[target_group]
+            # rows are to, columns are from
+            foi = np.dot(self.parms["beta"][target_group] , I_g / np.array(self.parms["N"]))
+            ifrac = 1.0 - np.exp(-foi * self.parms["dt"])
+            new_exposed.append(np.random.binomial(S, ifrac))
+        return new_exposed
+
+    def infectious(self, u): #need infection secondary
+        new_infectious = []
+        for group in range(self.groups):
+            S, E1, E2, I1, I2, R, Y = u[group]
+            efrac = 1.0 - math.exp(-self.parms["sigma"] * self.parms["dt"])
+            new_infectious.append(np.random.binomial(E, efrac))
+        return new_infectious
+
+    def recovery(self, u):
+        new_recoveries = []
+        for group in range(self.groups):
+            S, E1, E2, I1, I2, R, Y = u[group]
+            rfrac = 1.0 - math.exp(-self.parms["gamma"] * self.parms["dt"])
+            new_recoveries.append(np.random.binomial(I, rfrac))
+        return new_recoveries
+
+    def seirmodel(self, u, t): # add v group, possibly vaccinated group with vaccine failure going back eventually, might need more complexity for ongoing vaccine campaign
+        new_u = []
+        new_exposed = self.exposed(u)
+        old_exposed = self.exposed(u) #
+        new_infectious = self.infectious(u)# 
+        old_infectious = self.infectious(u)
+        new_recoveries = self.recovery(u)#
+        for group in range(self.groups):
+            S, E1, E2, I1, I2, R, Y = u[group]
+            new_S = S - new_exposed[group]
+            new_E1 = E1 + new_exposed[group] - old_exposed[group]
+            new_E2 = E2 + old_exposed[group] - new_infectious[group]
+            new_I1 = I1 + new_infectious[group] - old_infectious[group]
+            new_I2 = I2 + old_infectious[group] - new_recoveries[group]
+            new_R = R + new_recoveries[group]
+            new_Y = Y + new_exposed[group]
+            new_u.append([new_S, new_E1, new_E2, new_I1, new_I2, new_R, new_Y])
+        return new_u
