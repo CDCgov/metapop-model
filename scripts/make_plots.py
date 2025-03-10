b3ebc3eeb1c2ae767418a2b47afa3bf3168fb080
Paige Miller <yub1@cdc.gov>
2025-03-10 14:47:18 +0000

    changing x axis to make bins consistent

--- scripts/make_plots.py
+++ scripts/make_plots.py
@@ -6,7 +6,7 @@ import numpy as np
 results = pl.read_csv("output/results.csv")
 data = pl.read_csv("data/2025-02-28_TX_Epi_Report.csv")
 
-#### Visualization of one simulation
+#### Visualization of one simulation ####
 # Filter the data for replicate == 0 and initial_coverage_scenario == "low"
 replicate_0_results = results.filter((pl.col("replicate") == 0) & (pl.col("initial_coverage_scenario") == "low"))
 
@@ -24,7 +24,7 @@ plt.legend()
 plt.savefig('output/infection_curve.png')
 plt.close()
 
-#### Faceted Histogram
+#### Faceted Histogram ####
 # Filter the data for t=200
 filtered_results = results.filter(pl.col("t") == 200)
 
@@ -40,10 +40,9 @@ bins = np.linspace(min_value, max_value, 21)  # 20 bins
 g = sns.FacetGrid(filtered_results_df, row="initial_coverage_scenario", sharex=True, sharey=True, height=4, aspect=2)
 g.map_dataframe(sns.histplot, x="Y", hue="group", multiple="stack", bins=bins, palette=['blue', 'orange'])
 
-# Set the x-axis to log scale
-g.set(xscale="log")
+# Set the x-axis to log scale: g.set(xscale="log")
 
-g.set_axis_labels('Final size (log scale)', 'Number of simulations')
+g.set_axis_labels('Final size', 'Number of simulations')
 g.add_legend(title='Group')
 
 plt.savefig('output/final_size_log_scale.png')
