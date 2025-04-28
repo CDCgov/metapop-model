d8113642a48dd0588428d75bae29713e9f398848
Dina Mistry <uqx8@cdc.gov>
2025-04-28 17:09:45 +0000

    bumping version

--- metapop/version.py
+++ metapop/version.py
@@ -4,5 +4,5 @@ Version information for the metapop package.
 
 __all__ = ["__version__", "__versiondate__"]
 
-__version__ = "0.1.4"
-__versiondate__ = "2025-04-25"
+__version__ = "0.1.5"
+__versiondate__ = "2025-04-28"
