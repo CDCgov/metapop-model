497c980de356cf91c02c3668c8b0a5af11fe2c47
Dina Mistry <uqx8@cdc.gov>
2025-05-16 17:47:51 +0000

    bumping patch version - not quite a bug fix but a small change that will allow for tests to run on a branch regardless of merge conflicts in the PR - will monitor this change if it gets too costly

--- metapop/version.py
+++ metapop/version.py
@@ -4,5 +4,5 @@ Version information for the metapop package.
 
 __all__ = ["__version__", "__versiondate__"]
 
-__version__ = "0.3.0"
-__versiondate__ = "2025-05-15"
+__version__ = "0.3.1"
+__versiondate__ = "2025-05-16"
