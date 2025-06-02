000ef03449ab855a571b04f12d5e05fb94f5290d
Dina Mistry <uqx8@cdc.gov>
2025-06-02 22:14:31 +0000

    replacing hard coded first day vaccinations with function

--- metapop/sim.py
+++ metapop/sim.py
@@ -160,24 +160,43 @@ def simulate(parms, seed):
     #### Vaccinate the population on the first day if applicable
     # print("Vaccination uptake schedule:", parms["vaccination_uptake_schedule"])
     first_day = t_array[0]
-    if first_day in parms["vaccination_uptake_schedule"]:
-        new_vaccinated, new_vaccine_failures = model.vaccinate(u, t_array[0])
-        updated_susceptibles, updated_failures = model.get_updated_susceptibles(
-            u, new_vaccinated, new_vaccine_failures
-        )
-        # print("Updated susceptibles on day 1:", updated_susceptibles)
-        # print("Updated vaccine failures on day 1:", updated_failures)
+    S, V, SV, E1, E2, E1_V, E2_V, I1, I2, R, Y, X, u = vaccinate_on_day(
+        model,
+        u,
+        first_day,
+        groups,
+        S,
+        V,
+        SV,
+        E1,
+        E2,
+        E1_V,
+        E2_V,
+        I1,
+        I2,
+        R,
+        Y,
+        X,
+        parms["vaccination_uptake_schedule"],
+    )
+    # if first_day in parms["vaccination_uptake_schedule"]:
+    #     new_vaccinated, new_vaccine_failures = model.vaccinate(u, t_array[0])
+    #     updated_susceptibles, updated_failures = model.get_updated_susceptibles(
+    #         u, new_vaccinated, new_vaccine_failures
+    #     )
+    #     # print("Updated susceptibles on day 1:", updated_susceptibles)
+    #     # print("Updated vaccine failures on day 1:", updated_failures)
 
-        # Update the state vector with the new susceptibles and vaccine failures
-        for group in range(groups):
-            u[group][Ind.S.value] = updated_susceptibles[group]
-            u[group][Ind.SV.value] = updated_failures[group]
-            u[group][Ind.V.value] += new_vaccinated[group]
-            u[group][Ind.X.value] += new_vaccinated[group] + new_vaccine_failures[group]
-            S[0, group] = updated_susceptibles[group]
-            V[0, group] = u[group][Ind.V.value]
-            SV[0, group] = updated_failures[group]
-            X[0, group] = u[group][Ind.X.value]
+    #     # Update the state vector with the new susceptibles and vaccine failures
+    #     for group in range(groups):
+    #         u[group][Ind.S.value] = updated_susceptibles[group]
+    #         u[group][Ind.SV.value] = updated_failures[group]
+    #         u[group][Ind.V.value] += new_vaccinated[group]
+    #         u[group][Ind.X.value] += new_vaccinated[group] + new_vaccine_failures[group]
+    #         S[0, group] = updated_susceptibles[group]
+    #         V[0, group] = u[group][Ind.V.value]
+    #         SV[0, group] = updated_failures[group]
+    #         X[0, group] = u[group][Ind.X.value]
     # else:
     #    print("No vaccinations on the first day.")
     # print("Initial state vector:", u)
