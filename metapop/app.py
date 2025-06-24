4fbceac0e9c5ee33607dee4c3c8e4f5324520310
Dina Mistry <uqx8@cdc.gov>
2025-06-24 14:22:20 +0000

    notes added about the purpose of widget keys and correcting which key is used in advanced parameters editors

--- metapop/app.py
+++ metapop/app.py
@@ -149,6 +149,28 @@ def app(replicates=20):
         steps = get_step_values()
         helpers = get_helpers()
         formats = get_formats()
+
+        # --- Note on the use of widget id keys ---
+
+        # Widget id keys are defined for each of the shared parameters and the
+        # scenarios parameters. These are used to uniquely identify each
+        # interactive element in the sidebar which allows users to edit
+        # the model parameters used for simulations.
+
+        # Each interactive element must have its own unique key which gets
+        # passed on to the Streamlit session state.
+
+        # By providing these widgets with unique keys, we ensure that the values
+        # are correctly associated with the parameters they represent and the
+        # values can be reset when the user clicks the reset button. Pressing
+        # the reset results in the parameters being reset to their default values
+        # by looking for these keys in the Streamlit session state.
+
+        # If keys are not used, this means that the parameters will not be
+        # shown and modifiable by users in the sidebar panel. The No Interventions
+        # scenario parameters are not shown in the sidebar panel, so it is
+        # possible that the keys for this scenario are not used.
+
         keys0 = get_widget_idkeys(0)  # shared parameters
         keys1 = get_widget_idkeys(1)  # scenario 1
         keys2 = get_widget_idkeys(2)  # scenario 2
@@ -361,7 +383,7 @@ def app(replicates=20):
                 steps,
                 helpers,
                 formats,
-                keys1,
+                keys2,
                 disabled=False,
             )
 
