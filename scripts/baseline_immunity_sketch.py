f024d9469dac070c04b627fb6fc699636333ea71
Dina Mistry <dina.c.mistry@gmail.com>
2025-10-24 11:03:37 -0400

    refactoring some parts into separate methods
    * more functionalizing of code
    
    * wrapping up functions a little more
    
    * removing unneeded commented out lines

--- scripts/baseline_immunity_sketch.py
+++ scripts/baseline_immunity_sketch.py
@@ -124,8 +124,23 @@ def add_pop_range_to_df(df):
     return df
 
 
+def add_pop_percentage_to_df(df, pop_table):
+    # join on pop_table to get population values
+    df = df.with_columns(
+        (
+            pl.col("pop_text").map_elements(
+                lambda x: pop_table.filter(pl.col("population") == x)["percentage"][0],
+                return_dtype=pl.Float64,
+            )
+        ).alias("pop_percentage")
+    )
+    return df
+
+
 def add_threshold_coverage_to_df(df, vacc_table):
     # add threshold_coverage column
+    # this is the coverage values corresponding to the threshold_text
+    # and the coverage for the lower bound of the coverage range
     df = df.with_columns(
         (
             pl.col("threshold_text").map_elements(
@@ -165,281 +180,173 @@ def add_pop_fraction_in_coverage_range_to_df(df):
     # calculate fraction of population covered by coverage range
     expected_df = expected_df.with_columns(
         (pl.col("coverage_range_length") / pl.col("pop_range_length")).alias(
-            "fraction_population_covered"
+            "fraction_population_in_coverage_range"
         ),
     )
     return expected_df
+
+
+def build_initial_baseline_immunity_dataframe_from_user_inputs(
+    # coverage_range,
+    vacc_table,
+    pop_table,
+):
+    # get_coverage_distribution_mapping(pop_table, vacc_table)
+    # vacc_table = get_threshold_values(vacc_table)
+    cutoff_values = get_coverage_cutoffs(vacc_table)
+    # print("Cutoff values:", cutoff_values)
+    # print("Vacc table with numeric thresholds:")
+    # print(vacc_table)
+
+    coverage_range = get_coverage_ranges(cutoff_values)
+
+    # start building dataframe
+    df = pl.DataFrame(
+        {
+            "coverage_range": coverage_range,
+        }
+    )
+
+    # add matching threshold_text column
+    df = add_threshold_text_to_df(df, vacc_table)
+
+    # add matching pop_text column
+    df = add_pop_label_to_df(df, pop_table)
+
+    # add matching pop_range column
+    df = add_pop_range_to_df(df)
+
+    # add matching population percentage column
+    df = add_pop_percentage_to_df(df, pop_table)
+
+    # add matching threshold_coverage column
+    df = add_threshold_coverage_to_df(df, vacc_table)
+
+    # add coverage_range_min_age and coverage_range_max_age columns
+    df = add_coverage_range_min_max_to_df(df)
+
+    # add fraction_population_covered column
+    df = add_pop_fraction_in_coverage_range_to_df(df)
+
+    # remove unneeded columns
+    df = df.drop(
+        ["coverage_range_length", "pop_range_length", "coverage_range", "pop_range"]
+    )
+
+    print("columns in df:", df.columns)
+    return df
+
+
+def create_dataframe_for_baseline_immunity_calculation(df):
+    """"""
+    # make a copy of df
+    joined_df = df.clone()
+
+    # join on itself to get the threshold_coverage for the upper bound of the coverage age range
+    joined_df = joined_df.join(
+        expected_df[["coverage_range_min_age", "threshold_coverage"]],
+        left_on="coverage_range_max_age",
+        right_on="coverage_range_min_age",
+        how="full",
+        suffix="_right",
+    )
+
+    # remove row with null coverage_range
+    joined_df = joined_df.filter(
+        pl.col("coverage_range_min_age").is_not_null()
+        & pl.col("coverage_range_max_age").is_not_null()
+    )
+
+    # fill in threshold_coverage_right with threshold_coverage where null
+    # this is for the last row where there is no threshold_coverage in the
+    # original dataframe for the upper bound
+    joined_df = joined_df.with_columns(
+        pl.when(pl.col("threshold_coverage_right").is_null())
+        .then(pl.col("threshold_coverage"))
+        .otherwise(pl.col("threshold_coverage_right"))
+        .alias("threshold_coverage_right_filled"),
+    )
+
+    # add mid range coverage value for trapezoid rule
+    joined_df = joined_df.with_columns(
+        (
+            (pl.col("threshold_coverage") + pl.col("threshold_coverage_right_filled"))
+            / 2
+        ).alias("threshold_coverage_mid"),
+    )
+
+    # drop unneeded columns
+    joined_df = joined_df.drop(
+        ["coverage_range_min_age_right", "threshold_coverage_right"]
+    ).rename({"threshold_coverage_right_filled": "threshold_coverage_upper"})
+
+    # reorder columns
+    joined_df = joined_df.select(
+        [
+            "coverage_range_min_age",
+            "coverage_range_max_age",
+            "threshold_text",
+            "pop_text",
+            "fraction_population_in_coverage_range",
+            "pop_percentage",
+            "threshold_coverage",
+            "threshold_coverage_upper",
+            "threshold_coverage_mid",
+        ]
+    )
+
+    # return the joined dataframe that can be used for calulating the baseline immunity from age based inputs
+    return joined_df
+
+
+def calculate_baseline_immunity_from_dataframe(df):
+    df = df.with_columns(
+        (
+            pl.col("threshold_coverage_mid")
+            * pl.col("pop_percentage")
+            / 100
+            * pl.col("fraction_population_in_coverage_range")
+        ).alias("weighted_coverage")
+    )
+
+    immunity = df.select(pl.col("weighted_coverage")).sum()["weighted_coverage"][0]
+    return immunity
 
 
 config_path = os.path.join(
     os.path.dirname(__file__), "..", "metapop", "app_assets", "one_pop_config.yaml"
 )
 
-# print(config_path)
 
 parms = mp.read_parameters(config_path)
 
 pop_table = mp.initialize_pop_table(parms)
-pop_table = pop_table.with_columns(pl.Series("percentage", [100, 0, 0]))
-
 vacc_table = mp.initialize_vacc_table(parms)
 
-vacc_table = vacc_table.with_columns(pl.Series("coverage", [100, 100, 0, 0]))
 
-print(pop_table)
-print(vacc_table)
-
-# get population range mappings
+# get range mappings
 pop_table = add_pop_ranges_to_pop_table(pop_table)
-print(pop_table)
-
-# get_coverage_distribution_mapping(pop_table, vacc_table)
 vacc_table = get_threshold_values(vacc_table)
-cutoff_values = get_coverage_cutoffs(vacc_table)
-print("Cutoff values:", cutoff_values)
-print("Vacc table with numeric thresholds:")
+
+print(pop_table)
 print(vacc_table)
 
-coverage_range = get_coverage_ranges(cutoff_values)
-print("Coverage ranges:", coverage_range)
 
-# threshold_text = []
-# for i in coverage_range:
-#     val = get_threshold_label_from_coverage_range(i, vacc_table)
-#     # threshold_text.append(closest_row["threshold"])
-#     threshold_text.append(val)
-
-# print("Threshold text:", threshold_text)
-
-# start building dataframe
-df = pl.DataFrame(
-    {
-        "coverage_range": coverage_range,
-    }
-)
-
-# add threshold_text column
-# df = df.with_columns(
-#     (
-#         pl.col("coverage_range").map_elements(
-#             lambda x: get_threshold_label_from_coverage_range(x, vacc_table),
-#             return_dtype=pl.String,
-#         )
-#     ).alias("threshold_text")
-# )
-df = add_threshold_text_to_df(df, vacc_table)
-
-
-# add pop_text column
-# df = df.with_columns(
-#     (
-#         pl.col("coverage_range").map_elements(
-#             lambda x: get_pop_label_from_coverage(x, pop_table),
-#             return_dtype=pl.String,
-#         )
-#     ).alias("pop_text")
-# )
-df = add_pop_label_to_df(df, pop_table)
-
-
-# add pop_range column
-# df = df.with_columns(
-#     (
-#         pl.col("pop_text").map_elements(
-#             convert_pop_text, return_dtype=pl.List(pl.Int64)
-#         )
-#     ).alias("pop_range")
-# )
-df = add_pop_range_to_df(df)
-
-
-# add threshold_coverage column
-# df = df.with_columns(
-#     (
-#         pl.col("threshold_text").map_elements(
-#             lambda x: vacc_table.filter(pl.col("threshold") == x)["coverage"][0],
-#             return_dtype=pl.Float64,
-#         )
-#     ).alias("threshold_coverage")
-# )
-df = add_threshold_coverage_to_df(df, vacc_table)
-
-print("Real dataframe with threshold text:")
-print(df)
-
-
-# df = df.with_columns(
-#     pl.col("coverage_range").list.get(0).alias("coverage_range_min_age"),
-# )
-# df = df.with_columns(
-#     pl.col("coverage_range").list.get(1).alias("coverage_range_max_age"),
-# )
-df = add_coverage_range_min_max_to_df(df)
-
-expected_df = df.clone()
-
-
-# threshold_text = [
-#     "2 years",
-#     "2 years",
-#     "5 years (kindergarten)",
-#     "13 years",
-#     "18+",
-#     "18+",
-# ]
-
-# pop_ranges = [
-#     (0, 5),
-#     (0, 5),
-#     (0, 5),
-#     (5, 18),
-#     (5, 18),
-#     (18, 100),
-# ]
-
-# pop_text = [
-#     "<5",
-#     "<5",
-#     "<5",
-#     "5-17",
-#     "5-17",
-#     "18+",
-# ]
-
-# threshold_coverages = vacc_table["coverage"].to_list()
-# threshold_coverages = [0.0, 0.0] + threshold_coverages
-
-# expected_df = pl.DataFrame(
-#     {
-#         "coverage_range": coverage_range,
-#         "threshold_text": threshold_text,
-#         "pop_range": pop_ranges,
-#         "pop_text": pop_text,
-#         "threshold_coverage": threshold_coverages,
-#     }
-# )
-# print(expected_df)
-
-
-# expected_df = df.with_columns(
-#     pl.col("coverage_range").list.get(0).alias("coverage_range_min_age"),
-# )
-# expected_df = expected_df.with_columns(
-#     pl.col("coverage_range").list.get(1).alias("coverage_range_max_age"),
-# )
-
-
-# calculate coverage_range_length
-# expected_df = expected_df.with_columns(
-#     (pl.col("coverage_range_max_age") - pl.col("coverage_range_min_age")).alias(
-#         "coverage_range_length"
-#     ),
-# )
-
-# # calculate pop_range_length
-# expected_df = expected_df.with_columns(
-#     (pl.col("pop_range").list.get(1) - pl.col("pop_range").list.get(0)).alias(
-#         "pop_range_length"
-#     ),
-# )
-
-# # calculate fraction of population covered by coverage range
-# expected_df = expected_df.with_columns(
-#     (pl.col("coverage_range_length") / pl.col("pop_range_length")).alias(
-#         "fraction_population_covered"
-#     ),
-# )
-expected_df = add_pop_fraction_in_coverage_range_to_df(expected_df)
-
-
-# # reorder columns
-# expected_df = expected_df.select(
-#     [
-#         "coverage_range",
-#         "threshold_text",
-#         "pop_range",
-#         "pop_text",
-#         "pop_range_length",
-#         "fraction_population_covered",
-#         "coverage_range_min_age",
-#         "coverage_range_max_age",
-#         "coverage_range_length",
-#         "threshold_coverage",
-#     ]
-# )
-
-joined_df = expected_df.clone()
-joined_df = joined_df.join(
-    expected_df[["coverage_range_min_age", "threshold_coverage"]],
-    left_on="coverage_range_max_age",
-    right_on="coverage_range_min_age",
-    how="full",
-    suffix="_right",
-)
-
-# remove row with null coverage_range
-joined_df = joined_df.filter(
-    pl.col("coverage_range_min_age").is_not_null()
-    & pl.col("coverage_range_max_age").is_not_null()
-)
-
-# fill in threshold_coverage_right with threshold_coverage where null
-joined_df = joined_df.with_columns(
-    pl.when(pl.col("threshold_coverage_right").is_null())
-    .then(pl.col("threshold_coverage"))
-    .otherwise(pl.col("threshold_coverage_right"))
-    .alias("threshold_coverage_right_filled"),
-)
-
-# fill in mid coverage value
-joined_df = joined_df.with_columns(
-    (
-        (pl.col("threshold_coverage") + pl.col("threshold_coverage_right_filled")) / 2
-    ).alias("threshold_coverage_mid"),
-)
-
-# join on pop_table to get population values
-joined_df = joined_df.join(
+expected_df = build_initial_baseline_immunity_dataframe_from_user_inputs(
+    vacc_table,
     pop_table,
-    left_on="pop_text",
-    right_on="population",
-    how="left",
 )
 
 
-# drop unneeded columns
-joined_df = joined_df.drop(
-    ["coverage_range_min_age_right", "threshold_coverage_right"]
-).rename({"threshold_coverage_right_filled": "threshold_coverage_upper"})
+print("Expected df:")
+print(expected_df)
 
 
-# print("expected df:", expected_df)
+joined_df = create_dataframe_for_baseline_immunity_calculation(expected_df)
 
-# print(
-#     joined_df.select(
-#         pl.col("*").exclude(
-#             "pop_text",
-#             "threshold_text",
-#             "coverage_range",
-#             "pop_range",
-#             "pop_range_length",
-#             "coverage_range_length",
-#         )
-#     )
-# )
-# print("Excluding a single column:")
-# print("joined df:", joined_df.columns)
+print("joined df for baseline immunity calculation:")
+print(joined_df)
 
-# now some weighted calculations using the dataframe
 
-joined_df = joined_df.with_columns(
-    (
-        pl.col("threshold_coverage_mid")
-        * pl.col("percentage")
-        / 100
-        * pl.col("fraction_population_covered")
-    ).alias("weighted_coverage")
-)
-
-x = joined_df.select(pl.col("weighted_coverage")).sum()
-print("weighted coverage sum:", x["weighted_coverage"][0])
+immunity = calculate_baseline_immunity_from_dataframe(joined_df)
+print("Final baseline immunity value:", immunity)
